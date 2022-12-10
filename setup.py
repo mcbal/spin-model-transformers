ee5743e6740a7984bcdac2a011a950e5658b2b96
mcbal <matthiascbal@gmail.com>
2022-12-10 09:04:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="ising-transformers",
+    name="ising-transformers-jax",
     packages=find_packages(exclude=[""]),
     version="0.0.0",
     license="Apache License 2.0",
-    description="Ising Transformers",
+    description="Ising Transformers in JAX",
     author="Matthias Bal",
     author_email="matthiascbal@gmail.com",
     long_description_content_type="text/markdown",
-    url="https://github.com/mcbal/ising-transformers",
+    url="https://github.com/mcbal/ising-transformers-jax",
     install_requires=[
         "einops>=0.4",
         "equinox>=0.5",
