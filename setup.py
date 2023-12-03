c658f54660f219b2229805b31bab7dd0b004db28
mcbal <matthiascbal@gmail.com>
2023-12-03 22:03:06 +0100

    Bump install_requires versions

--- setup.py
+++ setup.py
@@ -18,11 +18,11 @@ setup(
     python_requires=">=3.9",
     install_requires=[
         "einops>=0.6.1",
-        "equinox>=0.10.11",
-        "jax>=0.4.13",
-        "jaxlib>=0.4.13",
+        "equinox>=0.11.2",
+        "jax>=0.4.20",
+        "jaxlib>=0.4.20",
         "jaxopt>=0.8",
-        "numpy",
+        "numpy>=1.26",
         "optax>=0.1.7",
     ],
     extras_require={
